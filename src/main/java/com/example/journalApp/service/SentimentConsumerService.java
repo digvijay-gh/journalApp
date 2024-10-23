936c2a32418970a895e9a6848d31ae6c0502ba1b
Digvijay Singh Yadav <digvijaydsy2@gmail.com>
2024-10-23 16:30:58 +0530

    uncommented consumer service

--- src/main/java/com/example/journalApp/service/SentimentConsumerService.java
+++ src/main/java/com/example/journalApp/service/SentimentConsumerService.java
@@ -1,24 +1,24 @@
-//package com.example.journalApp.service;
-//
-//import com.example.journalApp.model.SentimentData;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Service;
-//
-//import java.time.LocalDateTime;
-//
-//@Service
-//public class SentimentConsumerService {
-//
-//    @Autowired
-//    private EmailService mailService;
-//
-//    @KafkaListener(topics = "weekly-sentiments", groupId = "weekly-sentiment-group")
-//    public void consume(SentimentData sentimentData) {
-//        sendEmail(sentimentData);
-//    }
-//
-//    private void sendEmail(SentimentData sentimentData) {
-//        mailService.sendEmail(sentimentData.getEmail(),  "Sentiment for previous week", sentimentData.getSentiment()+ LocalDateTime.now());
-//    }
-//}
+package com.example.journalApp.service;
+
+import com.example.journalApp.model.SentimentData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class SentimentConsumerService {
+
+    @Autowired
+    private EmailService mailService;
+
+    @KafkaListener(topics = "weekly-sentiments", groupId = "weekly-sentiment-group")
+    public void consume(SentimentData sentimentData) {
+        sendEmail(sentimentData);
+    }
+
+    private void sendEmail(SentimentData sentimentData) {
+        mailService.sendEmail(sentimentData.getEmail(),  "Sentiment for previous week", sentimentData.getSentiment()+ LocalDateTime.now());
+    }
+}
